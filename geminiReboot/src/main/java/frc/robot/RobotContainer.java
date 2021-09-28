3517e62b525f7e3e2f5838870bdeab592f12dfbf
Alannah Napast <alannah.napast@gmail.com>
2021-09-28 16:07:17 -0400

    Add files via upload

---
+++ geminiReboot/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  public static XboxController driveController = new XboxController(0);
+  public static XboxController auxController = new XboxController(1);
+
+  public static Trigger leftTrigger = new Trigger();
+  public static Trigger rightTrigger = new Trigger();
+
+  public static Trigger auxLeftTrigger = new Trigger();
+  public static Trigger auxRightTrigger = new Trigger();
+  public static Trigger auxLDPad = new Trigger();
+  public static Trigger auxRDPad = new Trigger();
+  public static Trigger auxUpDPad = new Trigger();
+  public static Trigger auxDownDPad = new Trigger();
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {}
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+
+}
---
+++ geminiReboot/src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -0,0 +1,62 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+//import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot;
+import frc.robot.RobotContainer;
+
+/** An example command that uses an example subsystem. */
+public class ArcadeDrive extends CommandBase {
+  static boolean flip = false;
+	static boolean buttonState = false;
+	
+    public ArcadeDrive() {
+    	addRequirements(Robot.RobotDriveSystem);
+    }
+
+
+    public void initialize() {}
+
+    //Sets if the drive orientation is reversed
+    public static void setFlip(){
+    	flip = !flip;
+    }
+    
+    public void execute() {
+    	//Drives the robot in different orientations based on a driver button input
+    	if(flip){
+    		//High speed gear in front
+    		if(RobotContainer.driveController.getRawAxis(2) > 0.6){
+	        	Robot.RobotDriveSystem.drive(-RobotContainer.driveController.getY(Hand.kLeft) * 1.1, RobotContainer.driveController.getX(Hand.kLeft) * 0.8);
+	    	}else{
+	    	//Low speed gear in front
+	    		Robot.RobotDriveSystem.drive(-RobotContainer.driveController.getY(Hand.kLeft) * 0.6, RobotContainer.driveController.getX(Hand.kLeft) * 0.8);
+	    	}
+    	}
+    	else{
+    		//Overdrive
+	    	if(RobotContainer.driveController.getRawAxis(3) > 0.6){
+	    		Robot.RobotDriveSystem.drive(RobotContainer.driveController.getY(Hand.kLeft) * 1.1, RobotContainer.driveController.getX(Hand.kLeft) * 1.1);
+	    	//Fast Speed
+	    	}else if(RobotContainer.driveController.getRawAxis(2) > 0.6){
+	        	Robot.RobotDriveSystem.drive(RobotContainer.driveController.getY(Hand.kLeft) * 1.1, RobotContainer.driveController.getX(Hand.kLeft) * 0.8);
+	    	}else{
+    		//Low speed
+	    		Robot.RobotDriveSystem.drive(RobotContainer.driveController.getY(Hand.kLeft) * 0.6, RobotContainer.driveController.getX(Hand.kLeft) * 0.8);
+	    	}
+    	}
+    	//Low speed turning is faster because the turning was awful. High speed turning is slower because it was too sensitive
+        //#TokyoDrift
+    }
+
+    @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+    protected void end() {}
+
+    protected void interrupted() {}
+}
+
